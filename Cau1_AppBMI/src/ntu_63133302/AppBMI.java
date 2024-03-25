c0e37daf7c9a1dc5675db36afe562d78c41a873d
Ha Tu Huy <152080712+hatuhuy1398@users.noreply.github.com>
2024-03-25 22:08:52 +0700

    add panel parent

--- Cau1_AppBMI/src/ntu_63133302/AppBMI.java
+++ Cau1_AppBMI/src/ntu_63133302/AppBMI.java
@@ -1,5 +1,23 @@
 package ntu_63133302;
+import javax.swing.* ;
+import java.awt.* ;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.regex.Pattern;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 public class AppBMI {
+	public static void main(String[] args) {
+		
+		JFrame window = new JFrame("Midterm Project ");
+        TitledBorder winTitle = new TitledBorder("App BMI ") ;
+        winTitle.setTitleJustification(TitledBorder.CENTER);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(1000,600); 
+	}
 
 }
