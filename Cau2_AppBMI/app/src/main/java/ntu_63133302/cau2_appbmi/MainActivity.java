004d3e1cea9d9a448ebe9fdb73b8a1e4c2f00e41
Ha Tu Huy <152080712+hatuhuy1398@users.noreply.github.com>
2024-03-26 00:42:26 +0700

    declare variable into main activity java

--- Cau2_AppBMI/app/src/main/java/ntu_63133302/cau2_appbmi/MainActivity.java
+++ Cau2_AppBMI/app/src/main/java/ntu_63133302/cau2_appbmi/MainActivity.java
@@ -1,24 +1,26 @@
 package ntu_63133302.cau2_appbmi;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+    EditText edtTuoi;
+    EditText edtChieuCao;
+    EditText edtCanNang;
+
+    EditText edtKetQua;
+    Button btnTinh;
+    // group 2 radio
+
+    RadioGroup radioGroup;
+    RadioButton radNam;
+    RadioButton radNu;
+
 }
