ee4080c36006bd1e48bc57a7f09a0285c535b02b
Ha Tu Huy <152080712+hatuhuy1398@users.noreply.github.com>
2024-04-24 11:33:53 +0700

    done ex1

--- UseExplicitIntents/app/src/main/java/hatuhuy1398/useexplicitintents/SubActivityOne.java
+++ UseExplicitIntents/app/src/main/java/hatuhuy1398/useexplicitintents/SubActivityOne.java
@@ -1,11 +1,13 @@
 package hatuhuy1398.useexplicitintents;
 
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
 public class SubActivityOne extends AppCompatActivity {
     Button btn_sub_one;
     @Override
@@ -13,11 +15,11 @@ public class SubActivityOne extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_one);
         btn_sub_one = findViewById(R.id.btn_sub_one);
-        btn_sub_one.setOnClickListener(new View.OnClickListener(){
+        btn_sub_one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent iSubOne = new Intent(SubActivityOne.this, MainActivity.class);
-                startActivity(iSubOne);
+                Intent iHome = new Intent(SubActivityOne.this, MainActivity.class);
+                startActivity(iHome);
             }
         });
     }
