a1d9824b2f96b2c50480bc7af1e0ea38bd2344d8
Ha Tu Huy <152080712+hatuhuy1398@users.noreply.github.com>
2024-05-13 21:40:15 +0700

    ex buttom nav bar

---
+++ buttonnavigationbar/app/src/main/java/hatuhuy1398/buttonnavigationbar/MainActivity.java
@@ -0,0 +1,45 @@
+package hatuhuy1398.buttonnavigationbar;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import hatuhuy1398.buttonnavigationbar.databinding.ActivityMainBinding;
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+       binding.bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.profile:
+                    selectedFragment = new ProfileFragment();
+                    break;
+                case R.id.settings:
+                    selectedFragment = new SettingsFragment();
+                    break;
+            }
+            replaceFragment(selectedFragment);
+            return true;
+        });
+
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+    }
+}
