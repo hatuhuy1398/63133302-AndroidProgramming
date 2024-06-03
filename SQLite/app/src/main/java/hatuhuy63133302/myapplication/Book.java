d28146ef638980683dc21e0a0bdb2890049e544b
Ha Tu Huy <152080712+hatuhuy1398@users.noreply.github.com>
2024-06-03 18:18:22 +0700

    feat: sqlite

--- SQLite/app/src/main/java/hatuhuy63133302/myapplication/Book.java
+++ SQLite/app/src/main/java/hatuhuy63133302/myapplication/Book.java
@@ -1,2 +1,37 @@
-package hatuhuy63133302.myapplication;public class Book {
+package hatuhuy63133302.myapplication;
+
+public class Book {
+    private int bookID;
+    private String bookName;
+    private int page;
+    private float price;
+    private String description;
+
+    public Book(int bookID, String bookName, int page, float price, String description) {
+        this.bookID = bookID;
+        this.bookName = bookName;
+        this.page = page;
+        this.price = price;
+        this.description = description;
+    }
+
+    public int getBookID() {
+        return bookID;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
--- SQLite/app/src/main/java/hatuhuy63133302/myapplication/MainActivity.java
+++ SQLite/app/src/main/java/hatuhuy63133302/myapplication/MainActivity.java
@@ -1,24 +1,99 @@
 package hatuhuy63133302.myapplication;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView lv;
+    ArrayList<Book> books;
+    BookAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        lv = findViewById(R.id.lv);
+        books = new ArrayList<>();
+
+        SQLiteDatabase db = openOrCreateDatabase("DBSach.db", MODE_PRIVATE, null);
+        String sqlTaoBang = "CREATE TABLE IF NOT EXISTS Books( BookID integer PRIMARY KEY , " +
+                "BookName text, " +
+                "Page integer, " +
+                "Price Float, " +
+                "Description text);";
+        db.execSQL(sqlTaoBang);
+        insertSampleData(db);
+
+        retrieveData();
+
+        BookAdapter adapter = new BookAdapter(this, books);
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case DialogInterface.BUTTON_POSITIVE:
+                                deleteBook(position);
+                                break;
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                break;
+                        }
+                    }
+                };
+
+                android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Are you sure you want to delete this book?")
+                        .setPositiveButton("Yes", dialogClickListener)
+                        .setNegativeButton("No", dialogClickListener)
+                        .show();
+            }
         });
+    }
+
+    private void insertSampleData(SQLiteDatabase db) {
+        db.execSQL("INSERT INTO Books VALUES(5, 'Android', 100, 20000, 'Sách học lập trình Android')");
+        db.execSQL("INSERT INTO Books VALUES(6, 'iOS', 200, 30000, 'Sách học lập trình iOS')");
+        db.execSQL("INSERT INTO Books VALUES(7, 'PHP', 300, 40000, 'Sách học lập trình PHP')");
+    }
+
+
+
+    private void retrieveData() {
+        SQLiteDatabase db = openOrCreateDatabase("DBSach.db", MODE_PRIVATE, null);
+        String sqlSelect = "SELECT * FROM Books";
+        Cursor cursor = db.rawQuery(sqlSelect, null);
+        while (cursor.moveToNext()) {
+            int bookID = cursor.getInt(0);
+            String bookName = cursor.getString(1);
+            int page = cursor.getInt(2);
+            float price = cursor.getFloat(3);
+            String description = cursor.getString(4);
+            Book book = new Book(bookID, bookName, page, price, description);
+            books.add(book);
+        }
+        cursor.close();
+    }
+
+    private void deleteBook(int position) {
+        Book book = books.get(position);
+        SQLiteDatabase db = openOrCreateDatabase("DBSach.db", MODE_PRIVATE, null);
+        db.delete("Books", "BookID=?", new String[]{String.valueOf(book.getBookID())});
+        books.remove(position);
+        adapter.notifyDataSetChanged();
     }
 }
